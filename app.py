45cd11b82badec1e5fa574b4a18f8f3491893ac9
DOMINION JOHN <119686378+DOMINION-JOHN1@users.noreply.github.com>
2024-07-03 13:03:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import
 import os
 from langchain.schema import HumanMessage
 from langchain_openai import AzureChatOpenAI
